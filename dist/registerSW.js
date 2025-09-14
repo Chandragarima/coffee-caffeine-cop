a42f9feb8fa2f00ef7f7bba1cf35dab7fa5765fa
Chandragarima <garimachandra2020@u.northwestern.edu>
2025-09-14 13:52:21 -0700

    fix build

--- dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/coffee-caffeine-cop/sw.js', { scope: '/coffee-caffeine-cop/' })})}
--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <InstallPrompt />
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <BrowserRouter basename="/coffee-caffeine-cop" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<Ask />} />
             <Route path="/caffeine-tracker" element={<CaffeineTrackerPage />} />
